4a3a6d58065601f0ad639663a877e17ec8c9e2c0
Jelmer Mulder <jelmer@jelmermulder.dev>
2021-12-06 08:20:55 +0100

    Implement solutions for day 6

--- src/solutions/solution06.py
+++ src/solutions/solution06.py
@@ -1,6 +1,21 @@
 from solutions.solution import Solution
+from collections import Counter, defaultdict
 
 
 class Solution06(Solution):
     def solve(self, input_text):
-        pass
+        fish = list(map(int, input_text[0].split(',')))
+        fish_counts = defaultdict(int, Counter(fish).items())
+        return self.simulate(fish_counts, 80)
+
+    def simulate(self, fish, days):
+        for day in range(days):
+            new_fish = defaultdict(int)
+            for fish_value, fish_count in fish.items():
+                if fish_value > 0:
+                    new_fish[fish_value - 1] += fish_count
+                else:
+                    new_fish[6] += fish_count
+                    new_fish[8] += fish_count
+            fish = new_fish
+        return sum(fish.values())
--- src/solutions/solution06b.py
+++ src/solutions/solution06b.py
@@ -1,7 +1,10 @@
 from solutions.solution06 import Solution06
+from collections import Counter, defaultdict
 
 
 class Solution06B(Solution06):
 
     def solve(self, input_text):
-        pass
+        fish = list(map(int, input_text[0].split(',')))
+        fish_counts = defaultdict(int, Counter(fish).items())
+        return self.simulate(fish_counts, 256)
