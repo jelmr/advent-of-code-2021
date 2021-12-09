6e8630ac68cac0c30c88daf8ebdaaf3373ae77a4
Jelmer Mulder <jelmer@jelmermulder.dev>
2021-12-09 09:53:51 +0100

    Implement solutions for day 9

--- src/solutions/solution09.py
+++ src/solutions/solution09.py
@@ -1,6 +1,28 @@
 from solutions.solution import Solution
+import numpy as np
+from scipy.signal import convolve2d
+
+KERNELS = [
+    [[0, 1, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [1, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 1], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 1, 0]],
+]
 
 
 class Solution09(Solution):
     def solve(self, input_text):
-        pass
+        array = self.parse_input(input_text)
+        lowpoints = self.get_lowpoints(array)
+        return sum(array[lowpoints] + 1)
+
+    @staticmethod
+    def parse_input(input_text):
+        return np.array(list(map(list, input_text)), dtype=int)
+
+    @staticmethod
+    def get_lowpoints(array):
+        result = np.full(array.shape, True)
+        for kernel in KERNELS:
+            result &= convolve2d(array, kernel, boundary='fill', fillvalue=99, mode='same') > array
+        return result
--- src/solutions/solution09b.py
+++ src/solutions/solution09b.py
@@ -1,7 +1,31 @@
 from solutions.solution09 import Solution09
+import numpy as np
+import heapq
+from functools import reduce
+from operator import mul
 
 
 class Solution09B(Solution09):
 
     def solve(self, input_text):
-        pass
+        array = self.parse_input(input_text)
+        lowpoints = self.get_lowpoints(array)
+        largest_basins = []
+
+        for (x, y), is_lowpoint in np.ndenumerate(lowpoints):
+            if is_lowpoint:
+                size = self.recurse(array, np.full(array.shape, False), -1, x, y)
+                heapq.heappush(largest_basins, -size)
+
+        return reduce(mul, map(abs, heapq.nsmallest(3, largest_basins)))
+
+    def recurse(self, array, explored, origin, x, y):
+        in_bounds = 0 <= x < array.shape[0] and 0 <= y < array.shape[1]
+        if not in_bounds or explored[x, y] or array[x, y] <= origin or array[x, y] == 9:
+            return 0
+        val = array[x, y]
+        explored[x, y] = True
+        return 1 + self.recurse(array, explored, val, x + 1, y) + \
+               self.recurse(array, explored, val, x - 1, y) + \
+               self.recurse(array, explored, val, x, y + 1) + \
+               self.recurse(array, explored, val, x, y - 1)
