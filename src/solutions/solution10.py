f3b956d3391472766406ebe0a9ae3a4313137f79
Jelmer Mulder <jelmer@jelmermulder.dev>
2021-12-10 08:24:40 +0100

    Implement solutions for day 10

--- src/solutions/solution10.py
+++ src/solutions/solution10.py
@@ -1,6 +1,33 @@
 from solutions.solution import Solution
 
+OPEN_TO_CLOSE = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+    '<': '>',
+}
+CLOSE_TO_OPEN = {v: k for k, v in OPEN_TO_CLOSE.items()}
+POINTS = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
+
 
 class Solution10(Solution):
     def solve(self, input_text):
-        pass
+        return sum([self.check_line(line)[0] for line in input_text])
+
+    @staticmethod
+    def check_line(line):
+        operators = []
+        for c in line:
+            if c in OPEN_TO_CLOSE:
+                operators.append(c)
+            elif c in CLOSE_TO_OPEN and CLOSE_TO_OPEN[c] == operators[-1]:
+                operators.pop()
+            else:
+                return POINTS[c], operators
+
+        return 0, operators
--- src/solutions/solution10b.py
+++ src/solutions/solution10b.py
@@ -1,7 +1,27 @@
 from solutions.solution10 import Solution10
 
+POINTS = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4
+}
+
 
 class Solution10B(Solution10):
 
     def solve(self, input_text):
-        pass
+        scores = []
+        for line in input_text:
+            corrupt_score, operators = self.check_line(line)
+
+            if corrupt_score > 0:
+                continue
+
+            score = 0
+            for operator in operators[::-1]:
+                score *= 5
+                score += POINTS[operator]
+            scores.append(score)
+
+        return sorted(scores)[len(scores) // 2]
