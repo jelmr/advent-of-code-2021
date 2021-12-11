959396492572c788e0ebf51d5f863a49b0b5b9dc
Jelmer Mulder <jelmer@jelmermulder.dev>
2021-12-11 09:31:47 +0100

    Implement solutions for day 11

--- src/solutions/solution11.py
+++ src/solutions/solution11.py
@@ -1,6 +1,40 @@
 from solutions.solution import Solution
+import numpy as np
+from scipy.signal import convolve2d
+
+KERNEL = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
 
 
 class Solution11(Solution):
     def solve(self, input_text):
-        pass
+        grid = self.parse_input(input_text)
+        total_flashes = 0
+
+        for i in range(100):
+            total_flashes += self.simulate_step(grid)
+
+        return total_flashes
+
+    @staticmethod
+    def simulate_step(grid):
+        num_extra_flashes = 0
+        flashed = np.full(grid.shape, False)
+
+        grid += 1
+
+        while True:
+            to_flash = flashed ^ (grid > 9)
+            grid += convolve2d(to_flash.astype(int), KERNEL, boundary='fill', mode='same')
+            flashed |= to_flash
+            num_flashes_iteration = to_flash.sum()
+            num_extra_flashes += num_flashes_iteration
+            if num_flashes_iteration == 0:
+                break
+
+        grid[grid > 9] = 0
+
+        return num_extra_flashes
+
+    @staticmethod
+    def parse_input(input_text):
+        return np.array(list(map(list, input_text)), dtype=int)
--- src/solutions/solution11b.py
+++ src/solutions/solution11b.py
@@ -4,4 +4,10 @@ from solutions.solution11 import Solution11
 class Solution11B(Solution11):
 
     def solve(self, input_text):
-        pass
+        grid = self.parse_input(input_text)
+        step = 0
+
+        while self.simulate_step(grid) != grid.size:
+            step += 1
+
+        return step + 1
